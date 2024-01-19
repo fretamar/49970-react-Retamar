585ccdde07e591a45c1a4ea09c6792bce573fb4a
Francisco Retamar <fretamar@ml.com>
2024-01-19 15:21:34 -0300

    Agregue useRef

--- curso-react/src/Clase1/PanDulceValido.jsx
+++ curso-react/src/Clase1/PanDulceValido.jsx
@@ -1,5 +1,13 @@
+import {useRef} from "react"
 const PanDulceValido = () => {
     let producto = {imagen:"./src/assets/pan-dulce-frutas-valido.png.png" , titulo:"Pan dulce con frutas Valido - OFERTA" , precio:"750" , descripcion:"Ingredientes: Harina de trigo (enriquecida según Ley 25630*), frutas escurridas, oleomargarina, azúcar, huevos frescos, miel, levadura, emulsionante (mono y diflicéridos de ácidos grasos), sal, aromatizantes, saborizantes artificiales, lece en polvo, conservador (propionato de calico). Con conservador autorizado. Elaborado con oleomargarina, con esencias artificiales permitidas. Tenor graso: 9.9%."};
+
+    const cuenta = ""
+    const productoRef = useRef(null)
+
+    const modificarProducto = () => {
+        productoRef.current.innerHTML = "✅Agregado"
+    }
 
     return (
         <div className="card" style={{borderColor:"blue", backgroundColor:"#E7D4F5", width:"70%"}}>
@@ -11,6 +19,8 @@ const PanDulceValido = () => {
                     <h3>{producto.titulo}</h3>
                     <h5 className="font-italic" style={{color: "red", fontStyle: "bold"}}>$ {producto.precio} c/u</h5>
                     <p className="text-secondary" style={{fontStyle:"italic"}}>{producto.descripcion}</p>
+                    <button type="button" className="btn btn-primary " onClick={modificarProducto}>Agregar al carrito</button>
+                    <div ref={productoRef} style={{ fontStyle: "italic" }}>{cuenta}</div>
                 </div>
             </div>
         </div>
