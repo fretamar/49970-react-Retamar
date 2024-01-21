73a4ba51f3151764e16b542c5cfd2c7aec955efa
Francisco Retamar <franretamar123@gmail.com>
2024-01-21 16:42:23 -0300

    Agregue useRef

--- src/Componentes/BarritasMuecas.jsx
+++ src/Componentes/BarritasMuecas.jsx
@@ -1,20 +1,30 @@
+import { useRef } from "react"
+
 const BarritasMuecas = () => {
-let producto = {imagen:"./src/assets/barritas-muecas-todos-sabores.png.png" , titulo:"Barritas de cereal Müecas - OFERTA" , precio:"450" , descripcion:"Creemos que hacer alimentos y alimentarse puede ser algo lindo, rico y divertido. Eso es lo que intentamos transmitir. Más de 5 variedades."};
+    let producto = { imagen: "./src/assets/barritas-muecas-todos-sabores.png.png", titulo: "Barritas de cereal Müecas - OFERTA", precio: "450", descripcion: "Creemos que hacer alimentos y alimentarse puede ser algo lindo, rico y divertido. Eso es lo que intentamos transmitir. Más de 5 variedades." };
+
+    const modificaRef = useRef(null)
+    const mensajeBoton = () => {
+        modificaRef.current.innerHTML = "✅Producto agregado"
+        modificaRef.current.className = "text-secondary italic"
+    }
 
     return (
-        <div className="card" style={{borderColor:"blue", backgroundColor:"#E7D4F5", width:"70%"}}>
+        <div className="card" style={{ borderColor: "blue", backgroundColor: "#E7D4F5", width: "70%" }}>
             <div className="row text-center">
                 <div className="col-md-4 text-end">
                     <img src={producto.imagen} alt="Barritas de Cereal" />
                 </div>
                 <div className="col-md-8 py-2 text-start">
                     <h3>{producto.titulo}</h3>
-                    <h5 className="font-italic" style={{color: "red", fontStyle: "bold"}}>$ {producto.precio} c/u</h5>
-                    <p className="text-secondary" style={{fontStyle:"italic"}}>{producto.descripcion}</p>
+                    <h5 style={{ color: "red", fontStyle: "bold"  }}>$ {producto.precio} c/u</h5>
+                    <p className="text-secondary" style={{ fontStyle: "italic" }}>{producto.descripcion}</p>
+                    <button type="button" className="btn btn-primary" onClick={mensajeBoton}>Agregar al carrito</button>
+                    <p ref={modificaRef} style={{ fontStyle: "italic" }} ></p>
                 </div>
             </div>
         </div>
     )
-}  
+}
 
 export default BarritasMuecas;
--- src/Componentes/PanDulceValido.jsx
+++ src/Componentes/PanDulceValido.jsx
@@ -1,5 +1,13 @@
+import { useRef } from "react"
+
 const PanDulceValido = () => {
     let producto = {imagen:"./src/assets/pan-dulce-frutas-valido.png.png" , titulo:"Pan dulce con frutas Valido - OFERTA" , precio:"750" , descripcion:"Ingredientes: Harina de trigo (enriquecida según Ley 25630*), frutas escurridas, oleomargarina, azúcar, huevos frescos, miel, levadura, emulsionante (mono y diflicéridos de ácidos grasos), sal, aromatizantes, saborizantes artificiales, lece en polvo, conservador (propionato de calico). Con conservador autorizado. Elaborado con oleomargarina, con esencias artificiales permitidas. Tenor graso: 9.9%."};
+
+    const modificaRef = useRef(null)
+    const mensajeBoton = () => {
+        modificaRef.current.innerHTML = "✅Producto agregado"
+        modificaRef.current.className = "text-secondary italic"
+    }
 
     return (
         <div className="card" style={{borderColor:"blue", backgroundColor:"#E7D4F5", width:"70%"}}>
@@ -9,8 +17,10 @@ const PanDulceValido = () => {
                 </div>
                 <div className="col-md-8 py-2 text-start">
                     <h3>{producto.titulo}</h3>
-                    <h5 className="font-italic" style={{color: "red", fontStyle: "bold"}}>$ {producto.precio} c/u</h5>
+                    <h5 style={{color: "red", fontStyle: "bold"}}>$ {producto.precio} c/u</h5>
                     <p className="text-secondary" style={{fontStyle:"italic"}}>{producto.descripcion}</p>
+                    <button type="button" className="btn btn-primary" onClick={mensajeBoton}>Agregar al carrito</button>
+                    <p ref={modificaRef} style={{ fontStyle: "italic" }} ></p>
                 </div>
             </div>
         </div>
