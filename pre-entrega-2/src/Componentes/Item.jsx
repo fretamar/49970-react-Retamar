b24775b301f5a01963d0a73b75dda3bc481f8fba
Francisco Retamar <franretamar123@gmail.com>
2024-01-28 21:03:58 -0300

    pre-entrega-2

--- pre-entrega-2/src/Componentes/Item.jsx
+++ pre-entrega-2/src/Componentes/Item.jsx
@@ -5,7 +5,7 @@ import "../App.css"
 import ItemCount from "./ItemCount"
 import { Link } from "react-router-dom"
 
-const Item = ({ item }) => {
+const Item = () => {
     const [productos, setProductos] = useState([])
     const { categoria } = useParams()
 
@@ -26,18 +26,18 @@ const Item = ({ item }) => {
             {productos.map(produ => (
                 <div key={produ.id} style={{ paddingBottom: "5px" }}>
                     <div className="tarjeta-producto" style={{ borderColor: "blue", backgroundColor: "#E7D4F5" }}>
-                    <Link to={"/item/" + produ.id} >
                         <div className="row text-center">
+                            <Link to={"/item/" + produ.id} >
                                 <div>
                                     <img src={produ.foto} alt={produ.tipo} />
                                 </div>
                                 <div>
-                                    <h3 className="titulo-producto" style={{color:"black"}}>{produ.tipo} {produ.marca} {produ.formato}</h3>
+                                    <h3 className="titulo-producto" style={{ color: "black" }}>{produ.tipo} {produ.marca} {produ.formato}</h3>
                                     <h5 style={{ color: "red", fontStyle: "bold" }}>$ {produ.precio} c/u</h5>
                                 </div>
+                            </Link>
                             <ItemCount stock={19} />
                         </div>
-                        </Link>
                     </div>
                 </div>
             ))
